50dc9f632c1aef120d7888da9153561a849d1bee
Sebastian Otaegui <feniix@gmail.com>
2017-06-13 11:05:56 -0300

    reorg some stuff into classes

--- src/main/kotlin/cb/rc/app.kt
+++ src/main/kotlin/cb/rc/app.kt
@@ -7,13 +7,15 @@ import redis.clients.jedis.Jedis
 
 import org.nield.kotlinstatistics.*
 import java.util.stream.Collectors
-import java.lang.Double
+import java.util.concurrent.TimeUnit
+
 
 fun main(args: Array<String>) {
     val command = Command("check-rates", "command just for testing")
 
     command.addStringOption("currency", false, 'c', "Sets the currency: BTC or ETH. default: ETH")
     command.addIntegerOption("lbound", false, 'l', "Sets the USD that we are going to be checking for.")
+    command.addLongOption("check-delay", false, 'd', "Sets the delay between checks in minutes. default: 5")
 
     try {
         command.parse(args)
@@ -24,18 +26,12 @@ fun main(args: Array<String>) {
 
     val currency = command.getStringValue("currency", "ETH")
     val lbound = command.getIntegerValue("lbound", 50)
+    val delay = command.getLongValue("check-delay", 5) as Long
 
-    val jc = Jedis("localhost")
-    val median = jc.hgetAll("201706").values.sorted().stream().
-            map(Double::valueOf).
-            collect((Collectors.toList())).median()
+    while (true) {
+        println(coinbase().ethRate)
+        TimeUnit.MINUTES.sleep(delay)
+    }
 
-    println(median)
 
-//
-    //println(list)
-    //println(list.median())
-
-//    val usd = getCurrencyValue(currency)
-//    println(usd)
 }
---
+++ src/main/kotlin/cb/rc/coinbase.kt
@@ -0,0 +1,49 @@
+package cb.rc
+
+import com.github.salomonbrys.kotson.get
+import com.google.gson.JsonElement
+import mu.KotlinLogging
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import com.google.gson.JsonParser
+import java.math.BigDecimal
+
+private val logger = KotlinLogging.logger {}
+
+class coinbase() {
+    private val now = LocalDateTime.now()
+    private val payload  = JsonParser().parse(getCurrencyValue())
+
+    val fullDate: String?
+        get() = DateTimeFormatter.ofPattern("yyyyMMddHHmmss").format(now)
+    val date: String?
+        get() = DateTimeFormatter.ofPattern("yyyyMMdd").format(now)
+    val month: String?
+        get() = DateTimeFormatter.ofPattern("yyyyMMM").format(now)
+    val time: String?
+        get() = DateTimeFormatter.ofPattern("HHmm").format(now)
+    val ethRate: BigDecimal?
+        get() = calcRate(payload, "ETH")
+    val btcRate: BigDecimal?
+        get() = calcRate(payload, "BTC")
+    val ltcRate: BigDecimal?
+        get() = calcRate(payload, "LTC")
+
+    fun calcRate(payload: JsonElement, currency: String): BigDecimal? {
+        return BigDecimal.ONE.setScale(2,BigDecimal.ROUND_HALF_UP) / payload["data"]["rates"][currency].asBigDecimal
+    }
+
+    fun getCurrencyValue(): String? {
+        return try {
+            val r = khttp.get("https://api.coinbase.com/v2/exchange-rates")
+            logger.debug { r.statusCode }
+            when (r.statusCode) {
+                200 -> r.text
+                else -> null
+            }
+        } catch (e: Exception) {
+            logger.error { e }
+            null
+        }
+    }
+}
