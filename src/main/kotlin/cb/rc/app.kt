302078bdfd70d5a1836864e30c67ec01ca3ef203
Sebastian Otaegui <feniix@gmail.com>
2017-06-13 01:53:01 -0300

    some more prototyping

--- src/main/kotlin/cb/rc/app.kt
+++ src/main/kotlin/cb/rc/app.kt
@@ -3,7 +3,11 @@ package cb.rc
 import com.evalab.core.cli.Command
 import com.evalab.core.cli.exception.OptionException
 
-//import redis.clients.jedis.Jedis
+import redis.clients.jedis.Jedis
+
+import org.nield.kotlinstatistics.*
+import java.util.stream.Collectors
+import java.lang.Double
 
 fun main(args: Array<String>) {
     val command = Command("check-rates", "command just for testing")
@@ -18,11 +22,20 @@ fun main(args: Array<String>) {
         System.exit(2)
     }
 
-    val currency = command.getStringValue("currency", "ETH") as String
+    val currency = command.getStringValue("currency", "ETH")
     val lbound = command.getIntegerValue("lbound", 50)
 
+    val jc = Jedis("localhost")
+    val median = jc.hgetAll("201706").values.sorted().stream().
+            map(Double::valueOf).
+            collect((Collectors.toList())).median()
 
-    val usd = get_currency_value(currency)
-    println(usd)
+    println(median)
 
+//
+    //println(list)
+    //println(list.median())
+
+//    val usd = getCurrencyValue(currency)
+//    println(usd)
 }
--- src/main/kotlin/cb/rc/fn.kt
+++ src/main/kotlin/cb/rc/fn.kt
@@ -1,9 +1,19 @@
 package cb.rc
 
-fun get_currency_value(currency: String): String {
-    val r = khttp.get("https://api.coinbase.com/v2/exchange-rates?currency=$currency")
-    when (r.statusCode) {
-        200 -> return r.jsonObject.getJSONObject("data").getJSONObject("rates").getString("USD")
-        else -> return ""
+import mu.KotlinLogging
+
+private val logger = KotlinLogging.logger {}
+
+fun getCurrencyValue(currency: String? = "ETH"): String? {
+    return try {
+        val r = khttp.get("https://api.coinbase.com/v2/exchange-rates?currency=$currency")
+        logger.debug { r.statusCode }
+        when (r.statusCode) {
+            200 -> r.jsonObject.getJSONObject("data").getJSONObject("rates").getString("USD")
+            else -> null
+        }
+    } catch (e: Exception) {
+        logger.error { e }
+        null
     }
 }
